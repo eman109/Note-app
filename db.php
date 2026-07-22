4584084cd4363798cc59ac82cbebcbfc57026a61
eman109 <ehusseinmmo@gmail.com>
2026-07-22 15:05:11 +0300

    removed old files

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $conn = new mysqli("localhost", "root", "", "notes_db"); ->added it in config
-
-if($conn->connect_error){
-    die("oconnection failed: " . $conn->connect_error);
-}
-?>
--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "db.php";
-$id = $_GET['id'];
-$stmt = $conn->prepare("DELETE FROM notes WHERE id=?");
-$stmt->bind_param("i", $id);
-$stmt->execute();
-header("Location: index.php");
-exit();
-?>
